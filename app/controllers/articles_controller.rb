d82717e1eb6b6b78975e26bfda23499ac57842a1
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-04-21 18:39:31 +0200

    push

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,20 @@
 class ArticlesController < ApplicationController
+  # GET /articles
   def index
     @articles = Article.all
   end
+
+  # GET /articles/:id
   def show
     @article = Article.find(params[:id])
   end
-end
+
+  # GET /articles/new
+  def new
+    @article = Article.new
+  end
+
+  # POST /articles
+  def create
+    @article = Article.new(article_params)
+    if @
